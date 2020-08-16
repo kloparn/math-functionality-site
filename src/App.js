1e7e63e8cf454c1b3aed2d0b75c9a8becc55c9e3
kloparn <adam97.live.com@live.com>
2020-08-16 11:42:22 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./styles/default-theme";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <MainScreen>
+        <Container>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Container>
+      </MainScreen>
+    </ThemeProvider>
   );
-}
+};
+
+const MainScreen = styled.div`
+  background-color: ${(props) => props.theme.colors.main};
+  color: ${(props) => props.theme.colors.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Container = styled.header`
+  display: flex;
+  flex-direction: column;
+  font-size: 2rem;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
---
+++ src/styles/default-theme.js
@@ -0,0 +1,11 @@
+export const darkTheme = {
+  colors: {
+    main: "#162447",
+    secondary: "#1f4068",
+    third: "#1b1b2f",
+    forth: "#add8e6",
+    text: "#ffffff",
+    backgroundActive: "black",
+    shadowColor: "black",
+  },
+};
