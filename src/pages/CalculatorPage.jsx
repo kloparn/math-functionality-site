c785eec347dc72e3ce0474b07adcce3911173356
kloparn <adam97.live.com@live.com>
2020-08-27 16:02:40 +0200

    just fixed a text field that's managable by a constant calculation

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const CalculatorPage = () => {
+  const [calculation, setCalculation] = useState("");
+
+  function isLetter(ch) {
+    return isLowerCase(ch) || isUppercase(ch);
+  }
+
+  function isLowerCase(ch) {
+    return ch.length === 1 && ch >= "a" && ch <= "ö";
+  }
+
+  function isUppercase(ch) {
+    return ch.length === 1 && ch >= "A" && ch <= "Ö";
+  }
+
   return (
-    <div>
-      <h1>CalculatorPage</h1>
-    </div>
+    <layout>
+      <CalculationInput
+        type="text"
+        onChange={(e) => {
+          e.target.value.length > 0
+            ? isLetter(e.target.value[e.target.value.length - 1])
+              ? (e.target.value = e.target.value.substring(
+                  0,
+                  e.target.value.length - 1
+                ))
+              : (e.target.value = e.target.value)
+            : (e.target.value = e.target.value);
+          setCalculation(e.target.value);
+        }}
+      ></CalculationInput>
+    </layout>
   );
 };
+
+const CalculationInput = styled.input`
+  padding: 2rem;
+  color: black;
+`;
+
+const layout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default CalculatorPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../logo.svg";
 
 const HomePage = () => {
   document.title = "HomePage";
